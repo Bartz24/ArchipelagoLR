338a99ba254dfd5c402befa9749f06080e6fa1c1
Bartz24 <bartz24games@gmail.com>
2025-10-10 19:28:48 -0400

    LR updates

--- worlds/lrff13/RuleLogic.py
+++ worlds/lrff13/RuleLogic.py
@@ -1,6 +1,10 @@
 from typing import List
+from .Items import item_data_table
 
 
 def state_has_at_least(possible: List[bool], count: int) -> bool:
     # Returns true if at least count of the possible are true
     return possible.count(True) >= count
+
+def item_is_category(item_name : str, category : str) -> bool:
+    return item_data_table[item_name].category == category
